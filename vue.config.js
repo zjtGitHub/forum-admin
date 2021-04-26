e4b71277c5179082c92075b218427a9b559e2105
zhangjintong <947140669@qq.com>
2021-04-26 18:02:33 +0800

    添加iviewloader

--- vue.config.js
+++ vue.config.js
@@ -29,10 +29,23 @@ module.exports = {
   // see https://github.com/vuejs/vue-cli/blob/dev/docs/webpack.md
   // 如果你不需要使用eslint，把lintOnSave设为false即可
   lintOnSave: true,
+
   chainWebpack: config => {
     config.resolve.alias
       .set('@', resolve('src')) // key,value自行定义，比如.set('@@', resolve('src/components'))
       .set('_c', resolve('src/components'))
+    config.module
+      .rule('vue')
+      .test(/\.vue$/)
+      .use('iview-loader')
+      .loader('iview-loader')
+      .tap(options => {
+        return {
+          prefix: true,
+          ...options
+        }
+      })
+      .end()
   },
   // 设为false打包时不生成.map文件
   productionSourceMap: false
